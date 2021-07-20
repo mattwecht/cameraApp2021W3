700416555436eb2ed87bf6991b92b6e409ca77ab
Matt Wecht <mattwecht@gmail.com>
2021-07-20 13:13:09 -0400

    Initial Commit

---
+++ cameraApp2021W3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cameraApp2021W3
+//
+//  Created by Matthew Wecht on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
